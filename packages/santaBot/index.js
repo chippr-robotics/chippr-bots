14384b16d8c08a6ca751102d2bb2a076b5f2de87
dontpanic <cody.w.burns@gmail.com>
2019-12-16 03:12:32 +0000

    watson connected

--- packages/santaBot/index.js
+++ packages/santaBot/index.js
@@ -2,29 +2,51 @@ require('dotenv').config()
 
 var config = require('./config.js');
 
-var kafka = require('kafka');
-var producer = require('./kafkaProd.js');
-
-var Twitter = require('twitter');
+var fs = require('fs'),
+    path = require('path'),
+    Twitter = require('twitter'),
+    list = require("./lister"),
+    request = require('request');
 var T = new Twitter(config);
 
 // Set up your search parameters
 var params = {
-  q: '#Christmas',
+  q: '#christmaslights',
   count: 10,
   result_type: 'recent',
   lang: 'en'
 }
 
+//T.post('statuses/update', { status: '10 Days until Christmas!' }, 
+//  function(err, data, response) { 
+//  console.log(data) // If there is no error, proceed
+//});
+
+var download = function(uri, filename, callback){
+    request(uri).pipe(fs.createWriteStream('./img/'+filename)).on('close', callback);
+
+};
+
+function getter(){ 
 T.get('search/tweets', params, function(err, data, response) {
-  // If there is no error, proceed
-  if(!err){
-    // Loop through the returned tweets
+if(!err){
+// Loop through the returned tweets
     for(let i = 0; i < data.statuses.length; i++){
-      // Get the tweet Id from the returned data
-      let id = { id: data.statuses[i].id_str }
+      let id = { id: data.statuses[i].id_str }; 
+       let photo = data.statuses[i];
+//       console.log(photo.entities.media);
+      if(photo.entities.media != undefined) {
+     console.log(photo.entities.media[0].media_url);
+     var onList = list({ url: photo.entities.media[0].media_url, owners:['me'],threshold: 0.6});
+    try{
+    if(photo.entities.media[0].media_url != undefined){
+      download(photo.entities.media[0].media_uri, nice123.jpg);
+    }
+}
+catch(err) {console.error(err)};
+}
       // Try to Favorite the selected Tweet
-      T.post('favorites/create', id, function(err, response){
+      /* T.post('favorites/create', id, function(err, response){
         // If the favorite fails, log the error message
         if(err){
           console.log(err[0].message);
@@ -35,10 +57,13 @@ T.get('search/tweets', params, function(err, data, response) {
           let tweetId = response.id_str;
           console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
         }
-      });
+      });*/
     }
   } else {
     console.log(err);
   }
 })
 
+};
+
+getter();
