ee94da64a2bb8dbb6b2907838504a6f62f397ec9
Ronin <58588775+gitr0n1n@users.noreply.github.com>
2020-03-17 04:16:02 -0600

    era4, ecip1088
    Add var response
    Move ecip1017 call to era4
    Rename Thawing to IceAge
    Add ecip1088

--- packages/bridgette-api/twitter/forkTweet.js
+++ packages/bridgette-api/twitter/forkTweet.js
@@ -10,15 +10,16 @@ function postResponse(T, blockNumber, forks) {
     var days = Math.floor( distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor( ( distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000); if(distance > 0){ 
-    var response = [ 
-      "The " + forkName +" update will happen in " + (forkBlk - blocknumber) + " blocks.", 
-      "The " + forkName + " fork will be in " + days + " days," + hours +" hours,
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000); if(distance > 0){
+    var response = [
+      "The " + forkName +" update will happen in " + (forkBlk - blocknumber) + " blocks or ~" + days + " days.",
+      "The " + forkName + " fork will be in ~" + days + " days," + hours +" hours.",
+      "The " + forkName + " update goes live on block " + forkBlk + "; in ~" + days + " days," + hours +" hours.",
      return response[Math.floor(Math.random() * response.length)]; }
    else {
         return true;
  }
-}   
+}
 
 response[Math.floor(Math.random() * response.length)]; } else {
         return ""+ forkName + " is here!!!!"; var response = [
@@ -35,11 +36,11 @@ response[Math.floor(Math.random() * response.length)]; } else {
             var intro = canRes.excited[i].word;
         }
     }
-    
+
     log.debug('[dflow/controllers/quadPrime.js] possible responses: ' + response);
-    T.post('statuses/update', { 
-        status: intro + response[Math.floor(Math.random() * response.length)] },         
-        function(err, data, response) { 
+    T.post('statuses/update', {
+        status: intro + response[Math.floor(Math.random() * response.length)] },
+        function(err, data, response) {
             log.debug('[dflow/controllers/quadPrime.js] returned data: ' + data);
     });
 }
