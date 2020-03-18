287851db1f97539652260ef4a99a37a98a603d10
dontpanic <cody.w.burns@gmail.com>
2020-03-18 16:09:57 +0000

    bridgetteDb testing

--- packages/bridgetteDB/bridgetteDB.js
+++ packages/bridgetteDB/bridgetteDB.js
@@ -1,33 +1,64 @@
-var kotti = require("@chippr-bots/common/kotti");
-
+var { log } = require("@chippr-bots/common");
+var Web3 = require('web3');
 var  kvs = require("@chippr-bots/contracts/build/contracts/kvs")
 
 
-const ABI = kvs.abi;
-const kvsAddr = kvs.networks['6'].address;
-
-const kvsContract = new kotti.eth.Contract(ABI, kvsAddr);
-
-kotti.eth.net.isListening().then( res => {console.log(res)});
-
 module.exports = class bridgetteDB {
 
-    constructor( { accountAddress = process.env.BRIDGETTE_ADDRESS_KOTTI || null, 
-                   accountPasswd = process.env.BRIDGETTE_PW_KOTTI || null
+    constructor( {
+                   nodeAddr = process.env.NODEADDR || null,
+                   accountAddress = process.env.BRIDGETTE_ADDRESS_KOTTI || null, 
+                   accountPasswd = process.env.BRIDGETTE_PW_KOTTI || null,
+                   kvsAddr = process.env.kvsContract ||  kvs.networks['6'].address,
+                   DBKEY = process.env.DBKEY || null,
                 } ) {
+      //setup node connection
+      this.nodeAddr = nodeAddr;
+      this.web3 = new Web3(new Web3.providers.HttpProvider(this.nodeAddr));
+      //save account info
       this.accountAddress = accountAddress;
       this.accountPasswd = accountPasswd;
+      this.DBKEY = DBKEY;
+
+      //setup smart contract
+      this.ABI = kvs.abi;
+      this.kvsAddr = kvsAddr;
+      this.kvsContract = new this.web3.eth.Contract(this.ABI, this.kvsAddr);
+
+     // return connection information
+      log.info(
+        "BridgetteDB established \n" +
+        "node address: " + this.nodeAddr + "\n" +
+        "kvsAddr: " + this.kvsAddr + "\n" +
+        "user account: " + this.accountAddress + "\n" +
+        "user password: " + this.accountPasswd + "\n" +
+        "dbkey: " + this.DBKEY
+      );
+      this.web3.eth.net.isListening().then( res => {console.log(res)});
     }
 
-    async get( _key ) {
-     let key = kotti.utils.sha3(_key);
-      return await kvsContract.methods.store( key ).call();
+// util functions
+   unlock(){
+    this.web3.eth.personal.unlockAccount(this.accountAddress, this.accountPasswd, 600);
+    return true;
    }
 
+   getKey( _key ) {
+    return this.web3.utils.sha3(this.DBKEY + _key);
+   }
+
+//db functions
+   async get( _key ) {
+     let key = this.getKey( _key );
+     await this.kvsContract.methods.store( key ).call().then( res => {
+       return res;
+       })
+     }
+
    async set( _key, _value ) {
-     let key = kotti.utils.sha3(_key);
-     kotti.eth.personal.unlockAccount(this.accountAddress, this.accountPasswd, 600);
-      return await kvsContract.methods.set( key, _value ).send({
+     let key = this.getKey( _key );
+     await this.unlock();
+     return await this.kvsContract.methods.set( key, _value ).send({
         from: this.accountAddress,
         gas: '900000',
         gasPrice: '20000000000'
@@ -35,25 +66,14 @@ module.exports = class bridgetteDB {
    }
 
    async rem( _key ) {
-     let key = kotti.utils.sha3(_key);
-     kotti.eth.personal.unlockAccount(this.accountAddress, this.accountPasswd, 600);
-      return await kvsContract.methods.rem( key ).send({
+     let key = this.getKey( _key );
+     await this.unlock();
+     return await this.kvsContract.methods.rem( key ).send({
         from: this.accountAddress,
         gas: '90000',
         gasPrice: '20000000000'
       })
    }
-
- 
-
-
-
-
-
-
-
-
-
 }
 
 
--- packages/common/zecNode.js
+++ packages/common/zecNode.js
@@ -1,9 +1,4 @@
 var stdrpc = require('stdrpc');
-console.log({
-    url: process.env.ZEC_NODE,
-    username: process.env.ZEC_USER,
-    password: process.env.ZEC_PASS
-})
 module.exports = stdrpc({
     url: process.env.ZEC_NODE,
     username: process.env.ZEC_USER,
