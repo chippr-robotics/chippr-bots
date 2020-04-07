ad224968fc74a74e6f05e347909157cb31e36f19
dontpanic <cody.w.burns@gmail.com>
2020-04-07 14:55:16 +0000

    bridgetteDB add assert on types

--- packages/bridgetteDB/bridgetteDB.js
+++ packages/bridgetteDB/bridgetteDB.js
@@ -1,17 +1,35 @@
-var { log } = require("@chippr-bots/common");
-var Web3 = require('web3');
-var  kvs = require("@chippr-bots/contracts/build/contracts/kvs")
-
+const { log } = require("@chippr-bots/common");
+const Web3 = require('web3');
+const kvs = require("@chippr-bots/contracts/build/contracts/kvs")
+const EventEmitter = require("events");
+const assert = require("assert");
 
 module.exports = class bridgetteDB {
 
+    /**
+     * Connects to the blockchain and returns a new BDB object
+     * @param {Object} config - The optional config object
+     * @param {String} config.nodeAddr - The address of the ethereum node
+     * @param {String} config.accountAddress - The account to use on the node to send transactions
+     * @param {String} config.accountPassword - The password of the account to be used
+     * @param {String} config.kvsAddr - The contract address of the kvs on chain
+     * @param {String} config.DBKEY - The key to id the db, should be unique
+     */
+
     constructor( {
-                   nodeAddr = process.env.NODEADDR || null,
-                   accountAddress = process.env.BRIDGETTE_ADDRESS_KOTTI || null, 
-                   accountPasswd = process.env.BRIDGETTE_PW_KOTTI || null,
-                   kvsAddr = process.env.kvsContract ||  kvs.networks['6'].address,
-                   DBKEY = process.env.DBKEY || null,
-                } ) {
+        nodeAddr = process.env.NODEADDR || null,
+        accountAddress = process.env.BRIDGETTE_ADDRESS_KOTTI || null, 
+        accountPasswd = process.env.BRIDGETTE_PW_KOTTI || null,
+        kvsAddr = process.env.kvsContract ||  kvs.networks['6'].address,
+        DBKEY = process.env.DBKEY || null,
+     } = {}) {
+
+	assert.equal(typeof nodeAddr, "string");
+        assert.equal(typeof accountAddress, "string");
+        assert.equal(typeof accountPasswd,"string");
+        assert.equal(typeof kvsAddr,"string");
+	assert.equal(typeof DBKEY,"string");
+
       //setup node connection
       this.nodeAddr = nodeAddr;
       this.web3 = new Web3(new Web3.providers.HttpProvider(this.nodeAddr));
