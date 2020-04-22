764dea3729dc3e98de0a6547f101c35b41332948
Burns, Cody <cody.burns@accenture.com>
2020-04-22 12:50:50 -0500

    fixed tests to passing

---
+++ packages/bridgette-v1/test/index.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+require('dotenv').config();
+//* unit tests *//
+
+var env = require('./unit/env');
+
+//* end unit tests *//
+
+/* tests */
+describe('Global functions', function() {
+  env
+});
