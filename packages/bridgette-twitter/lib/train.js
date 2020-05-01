d719820869a33fdce8b2e7e0609269585f176f54
Burns, Cody <cody.burns@accenture.com>
2020-05-01 12:21:38 -0500

    cleaning up training model and rm showing password in bdb

--- packages/bridgette-twitter/lib/train.js
+++ packages/bridgette-twitter/lib/train.js
@@ -4,15 +4,15 @@ var { log } = require('@chippr-bots/common');
 */
 
 module.exports = (tweetstack, category, model) => {
-    console.log(tweetstack.length);
+    //console.log(tweetstack.length);
     model.exampleCount += tweetstack.length;
     for(tweet in tweetstack){
-        log.info(`[bridgette-twitter/lib/train] tweet number: ${tweet}`);
-        console.log(tweetstack[tweet]);
+        log.info(`[bridgette-twitter/lib/train] training on tweet number: ${tweet} out of  ${tweetstack.length}`);
+        //console.log(tweetstack[tweet]);
         for( i in tweetstack[tweet]){ 
             let word = tweetstack[tweet][i];
-            console.log(word);
-            console.log(model.wordlist[word]);
+            //console.log(word);
+            //console.log(model.wordlist[word]);
             if(model.wordlist[word] == undefined){
                 model.wordlist[word] = 1;
                 model.wordCount += 1;
@@ -23,6 +23,16 @@ module.exports = (tweetstack, category, model) => {
             }
         }
     }
-    console.log(model);
+    //remove all low occurence words: occurs less than 3 times or has a length of 1 to elimanante  
+    for (key in model.wordlist){
+        let modelKeys = Object.keys(model.wordlist);
+        console.log(modelKeys[key]);
+        if( model.wordlist[key] != undefined && (model.wordlist[key] <= 3 || modelKeys[key].length == 1)) {
+            delete model.wordlist[key];
+            model.wordCount -= 1;
+            model.vocabCount -= 1
+        }
+    }
+    //console.log(model);
     return model;
 }
