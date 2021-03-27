4b8f490e5a875d7c9ed975e484296250dce0ebaa
dontpanic <cody.w.burns@gmail.com>
2021-03-27 12:39:59 +0000

    2 line working

--- packages/text-shrink/shrink.py
+++ packages/text-shrink/shrink.py
@@ -2,7 +2,7 @@ from nltk.corpus import stopwords
 from nltk.cluster.util import cosine_distance
 import numpy as np
 import networkx as nx
- 
+
 def read_article(file_name):
     file = open(file_name, "r")
     filedata = file.readlines()
@@ -12,40 +12,40 @@ def read_article(file_name):
     for sentence in article:
         print(sentence)
         sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
-    sentences.pop() 
-    
+    sentences.pop()
+
     return sentences
 
 def sentence_similarity(sent1, sent2, stopwords=None):
     if stopwords is None:
         stopwords = []
- 
+
     sent1 = [w.lower() for w in sent1]
     sent2 = [w.lower() for w in sent2]
- 
+
     all_words = list(set(sent1 + sent2))
- 
+
     vector1 = [0] * len(all_words)
     vector2 = [0] * len(all_words)
- 
+
     # build the vector for the first sentence
     for w in sent1:
         if w in stopwords:
             continue
         vector1[all_words.index(w)] += 1
- 
+
     # build the vector for the second sentence
     for w in sent2:
         if w in stopwords:
             continue
         vector2[all_words.index(w)] += 1
- 
+
     return 1 - cosine_distance(vector1, vector2)
- 
+
 def build_similarity_matrix(sentences, stop_words):
     # Create an empty similarity matrix
     similarity_matrix = np.zeros((len(sentences), len(sentences)))
- 
+
     for idx1 in range(len(sentences)):
         for idx2 in range(len(sentences)):
             if idx1 == idx2: #ignore if both are same sentences
@@ -76,7 +76,7 @@ def generate_summary(file_name, top_n=5):
     for i in range(top_n):
       summarize_text.append(" ".join(ranked_sentence[i][1]))
 
-    # Step 5 - Offcourse, output the summarize texr
+    # Step 5 - Ofcourse, output the summarize text
     print("Summarize Text: \n", ". ".join(summarize_text))
 
 # let's begin
